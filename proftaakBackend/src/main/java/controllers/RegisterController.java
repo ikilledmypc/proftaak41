a430b3ea5dd750ac103c9e8653744f49093bae77
Tim Smeets <timsmeets@rocketmail.com>
2014-09-16 12:09:30 +0200

    begin gemaakt voor registratie

--- proftaakBackend/src/main/java/controllers/RegisterController.java
+++ proftaakBackend/src/main/java/controllers/RegisterController.java
@@ -1,15 +1,25 @@
 package controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import domain.Account;
 
 
 @RestController
 public class RegisterController {
-	@RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+	@RequestMapping("/register")
+    public Account register(
+    		@RequestParam(value="username", required=true) String username,
+    		@RequestParam(value="name", required= true)String name,
+    		@RequestParam(value="address", required= true)String address,
+    		@RequestParam(value="zipcode", required = true)String zipcode,
+    		@RequestParam(value="city", required = true)String city,
+    		@RequestParam(value="email", required = true)String email,
+    		@RequestParam(value="telephone", required = true)String telephone) {
+		
+		return null;
     }
 
 }
