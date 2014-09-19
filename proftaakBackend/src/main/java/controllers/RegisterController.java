d760b01922fa947f13b1bdba65f8b8b07e83bdf9
timsmeets@rocketmail.com <timsmeets@rocketmail.com>
2014-09-19 02:05:27 +0200

    forgot to save before commit see previous

--- proftaakBackend/src/main/java/controllers/RegisterController.java
+++ proftaakBackend/src/main/java/controllers/RegisterController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import managers.JsonManager;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +15,9 @@ public class RegisterController {
 	
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String logs(@RequestParam("json") String json) {
-		
+		JsonManager jsonManager = JsonManager.GetInstance();
+		Account account = (Account) jsonManager.fromJson(json, Account.class.getClass());
+		account.register();
 	    return null;
 	}
 
--- proftaakBackend/src/main/java/interfaces/IDatabase.java
+++ proftaakBackend/src/main/java/interfaces/IDatabase.java
@@ -3,7 +3,6 @@ package interfaces;
 import java.sql.ResultSet;
 
 public interface IDatabase {
-	public abstract IDatabase getInstance();
 	public ResultSet select(String s);
 	public boolean insert(String s);
 	public boolean delete(String s);
