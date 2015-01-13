a8655c231f85a7fb7c717a90ce3f824d9f2bf093
Tim <timsmeets@rocketmail.com>
2015-01-13 13:46:53 +0100

    product updated

--- FrontEnd/src/domain/Product.java
+++ FrontEnd/src/domain/Product.java
@@ -1,6 +1,33 @@
 package domain;
 
+import java.util.logging.Logger;
+
 public class Product {
+
+    private int productID;
+    private String name;
+    private int amount;
+    private Photo photo;
+    private boolean sepia;
+    private boolean blackWhite;
+    private int cropX;
+    private int cropY;
+    private int cropWidth;
+    private int cropHeight;
+
+    public Product(int productID, String name, int amount, Photo photo, boolean sepia, boolean blackWhite, int cropX, int cropY, int cropWidth, int cropHeight, float materialPrice) {
+        this.productID = productID;
+        this.name = name;
+        this.amount = amount;
+        this.photo = photo;
+        this.sepia = sepia;
+        this.blackWhite = blackWhite;
+        this.cropX = cropX;
+        this.cropY = cropY;
+        this.cropWidth = cropWidth;
+        this.cropHeight = cropHeight;
+        this.materialPrice = materialPrice;
+    }
 
     public void setAmount(int amount) {
         this.amount = amount;
@@ -14,18 +41,44 @@ public class Product {
         this.photo = photo;
     }
 
+    public Product(int productID, String name, float materialPrice) {
+        super();
+        this.productID = productID;
+        this.name = name;
+        this.materialPrice = materialPrice;
+        this.amount = 1;
+    }
 
-	public Product(int productID, String name, float materialPrice) {
-		super();
-		this.productID = productID;
-		this.name = name;
-		this.materialPrice = materialPrice;
-                this.amount =1;
-	}
+    /**
+     * Constructor
+     *
+     * @param name
+     * @param materialPrice
+     */
+    public Product(String name, float materialPrice) {
+        super();
+        this.name = name;
+        this.materialPrice = materialPrice;
+        this.amount = 1;
+    }
 
-	private int productID;
-	private String name;
-        private int amount;
+    public Product(int productID, String name, float materialPrice, Photo photo) {
+        super();
+        this.productID = productID;
+        this.name = name;
+        this.materialPrice = materialPrice;
+        this.photo = photo;
+        this.amount = 1;
+    }
+
+    public Product(String name, float materialPrice, Photo foto) {
+        super();
+        this.name = name;
+
+        this.materialPrice = materialPrice;
+        this.photo = foto;
+        this.amount = 1;
+    }
 
     public int getAmount() {
         return amount;
@@ -34,96 +87,109 @@ public class Product {
     public void addAmount(int amount) {
         this.amount += amount;
     }
-	private float materialPrice;
-	public Photo getPhoto() {
-		return photo;
-	}
+    private float materialPrice;
 
-	private Photo photo;
+    public Photo getPhoto() {
+        return photo;
+    }
+    
+        public boolean isSepia() {
+        return sepia;
+    }
 
-	/**
-	 * Constructor
-	 * 
-	 * @param name
-	 * @param materialPrice
-	 */
-	public Product(String name, float materialPrice) {
-		super();
-		this.name = name;
-		this.materialPrice = materialPrice;
-                 this.amount =1;
-	}
-	public Product(int productID, String name, float materialPrice,Photo photo) {
-		super();
-		this.productID = productID;                
-		this.name = name;
-		this.materialPrice = materialPrice;
-		this.photo = photo;
-                 this.amount =1;
-	}
+    public void setSepia(boolean sepia) {
+        this.sepia = sepia;
+    }
 
+    public boolean isBlackWhite() {
+        return blackWhite;
+    }
 
-	public Product(String name, float materialPrice, Photo foto) {
-		super();
-		this.name = name;
-                
-		this.materialPrice = materialPrice;
-		this.photo = foto;
-                 this.amount =1;
-	}
-	/**
-	 * @return the materialPrice
-	 */
-	public float getMaterialPrice() {
-		return materialPrice;
-	}
-	
-	public void setPhoto(Photo photo) {
-		this.photo = photo;
-	}
-        
+    public void setBlackWhite(boolean blackWhite) {
+        this.blackWhite = blackWhite;
+    }
 
+    public int getCropX() {
+        return cropX;
+    }
 
-	/**
-	 * @param materialPrice
-	 *            the materialPrice to set
-	 */
-	public void setMaterialPrice(float materialPrice) {
-		this.materialPrice = materialPrice;
-	}
+    public void setCropX(int cropX) {
+        this.cropX = cropX;
+    }
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    public int getCropY() {
+        return cropY;
+    }
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setCropY(int cropY) {
+        this.cropY = cropY;
+    }
 
-	/**
-	 * @return the productID
-	 */
-	public int getProductID() {
-		return productID;
-	}
+    public int getCropWidth() {
+        return cropWidth;
+    }
 
-	/**
-	 * @param productID
-	 *            the productID to set
-	 */
-	public void setProductID(int productID) {
-		this.productID = productID;
-	}
-        
-        @Override
-        public String toString(){
-            return this.name + " \u20ac "+this.materialPrice;
-        }
+    public void setCropWidth(int cropWidth) {
+        this.cropWidth = cropWidth;
+    }
+
+    public int getCropHeight() {
+        return cropHeight;
+    }
+
+    public void setCropHeight(int cropHeight) {
+        this.cropHeight = cropHeight;
+    }
+    
+
+    /**
+     * @return the materialPrice
+     */
+    public float getMaterialPrice() {
+        return materialPrice;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
+
+    /**
+     * @param materialPrice the materialPrice to set
+     */
+    public void setMaterialPrice(float materialPrice) {
+        this.materialPrice = materialPrice;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the productID
+     */
+    public int getProductID() {
+        return productID;
+    }
+
+    /**
+     * @param productID the productID to set
+     */
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " \u20ac " + this.materialPrice;
+    }
 }
